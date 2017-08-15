ccf55816cf5ae3137da59012fdf1c27d0e56760d
Łukasz Gałka <lukasz.galka@intive.com>
2017-08-15 09:19:39 +0200

    enable vertical scroll for gallery with swipe

--- lib/ngx-gallery.module.ts
+++ lib/ngx-gallery.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
 import { NgxGalleryArrowsComponent } from './ngx-gallery-arrows.component';
 import { NgxGalleryImageComponent } from './ngx-gallery-image.component';
 import { NgxGalleryThumbnailsComponent } from './ngx-gallery-thumbnails.component';
 import { NgxGalleryPreviewComponent } from './ngx-gallery-preview.component';
 import { NgxGalleryComponent } from './ngx-gallery.component';
+
+export class CustomHammerConfig extends HammerGestureConfig  {
+    overrides = <any>{
+        'pinch': { enable: false },
+        'rotate': { enable: false }
+    }
+}
 
 @NgModule({
     imports: [
@@ -20,6 +29,12 @@ import { NgxGalleryComponent } from './ngx-gallery.component';
     ],
     exports: [
         NgxGalleryComponent
+    ],
+    providers: [
+        {
+            provide: HAMMER_GESTURE_CONFIG,
+            useClass: CustomHammerConfig
+        }
     ]
 })
 export class NgxGalleryModule { }
