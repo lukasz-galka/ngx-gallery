58617caa487415cbf4d5ecfde4d3c1c3750bb90f
Javier Martinez <javiermarsanweb@gmail.com>
2020-05-06 18:26:09 +0200

    Update to Angular 9

--- src/ngx-gallery-preview.component.ts
+++ src/ngx-gallery-preview.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, ElementRef, HostListener, ViewChild, Renderer } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, ElementRef, HostListener, ViewChild, Renderer2 } from '@angular/core';
 import { SafeResourceUrl, DomSanitizer, SafeUrl, SafeStyle } from '@angular/platform-browser';
 
 import { NgxGalleryAction } from './ngx-gallery-action.model';
@@ -100,7 +100,7 @@ export class NgxGalleryPreviewComponent implements OnInit, OnChanges {
     private keyDownListener: Function;
 
     constructor(private sanitization: DomSanitizer, private elementRef: ElementRef,
-        private helperService: NgxGalleryHelperService, private renderer: Renderer,
+        private helperService: NgxGalleryHelperService, private renderer: Renderer2,
         private changeDetectorRef: ChangeDetectorRef) {}
 
     ngOnInit(): void {
@@ -160,7 +160,7 @@ export class NgxGalleryPreviewComponent implements OnInit, OnChanges {
             this.manageFullscreen();
         }
 
-        this.keyDownListener = this.renderer.listenGlobal("window", "keydown", (e) => this.onKeyDown(e));
+        this.keyDownListener = this.renderer.listen("window", "keydown", (e) => this.onKeyDown(e));
     }
 
     close(): void {
